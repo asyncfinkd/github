367143196da409a9f890c5d25e3311efb017542c
asyncfinkd <shamgoingzuck@gmail.com>
2021-09-29 13:53:55 +0400

    Spinner, serveralerror in repos page

--- src/pages/repos/ReposPages.tsx
+++ src/pages/repos/ReposPages.tsx
@@ -1,23 +1,44 @@
-import axios from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 import React, { useState, useEffect } from "react";
 import Container from "../../helper/Container";
 import Lists from "../../helper/Lists";
+import Progress from "../../helper/Progress";
 
 const ReposPages: React.FC = () => {
   const [data, setData] = useState<any>([]);
+  const [spinner, setSpinner] = useState<boolean>(true);
+  const [serveralError, setServeralError] = useState<boolean>(false);
   useEffect(() => {
     let newPath = window.location.pathname.split("/");
     axios
       .get(`https://api.github.com/users/${newPath[2]}/repos`)
-      .then((res: any) => {
-        setData(res.data);
+      .then((response: AxiosResponse) => {
+        setData(response.data);
+        setSpinner(false);
+      })
+      .catch((reason: AxiosError) => {
+        if (reason.response!.status !== 400) {
+          setServeralError(true);
+          setSpinner(false);
+        }
       });
   }, []);
   return (
     <>
-      <Container onScreen={false}>
-        <Lists data={data} />
-      </Container>
+      {serveralError ? (
+        <p>404 error, user is not defined</p>
+      ) : (
+        <>
+          {spinner && (
+            <Container onScreen={true}>
+              <Progress spinner={spinner} />
+            </Container>
+          )}
+          <Container onScreen={false}>
+            {spinner !== true && <Lists data={data} />}
+          </Container>
+        </>
+      )}
     </>
   );
 };
